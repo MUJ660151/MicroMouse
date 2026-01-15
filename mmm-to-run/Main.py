db6eb8ca8cdb76e148f533c7e360bd4473ea9e47
MUJ660151 <Maximilliamu@gmail.com>
2026-01-15 11:10:18 -0800

    proper wall tracking, relative limits, uses orientation offsets & 2 direction lists, added within limits method

--- mmm-to-run/Main.py
+++ mmm-to-run/Main.py
@@ -48,8 +48,8 @@ def floodFill(image):
     #TODO make queue work with any dimension maze
     queue = deque([(15, 15), (17, 17), (17, 15), (15, 17)]) #note: the order for these matters 
     #TODO find best order for queue
-
-    directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    #Visited = [[False for _ in range(2*mWidth+1)] for _ in range(2*mHeight+1)]
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     #               up      right   down     left
 
     #TODO: make tile values change based on discovered walls
@@ -58,84 +58,127 @@ def floodFill(image):
         for dr, dc in directions:
             nr, nc = r + 2*dr, c + 2*dc #floor tile coords in direction
             wr, wc = r + dr, c + dc #wall tile coords in direction
-
             if 0 <= nr <= 2*mWidth and 0 <= nc <= 2*mHeight and image[nr][nc] > image[r][c]+1:
                 if image[wr][wc] == noWallVal:
+                    #Visited[nr][nc] = True
                     image[nr][nc] = image[r][c] + 1
                     queue.append((nr, nc))
-                    API.setText(int((nr)/2), int((nc)/2), image[nr][nc])
+                    API.setText(int((nc-1)/2), 15-int((nr-1)/2), image[nr][nc])
     return image
+def addWall(x, y, direction):
+
+    pass
+
+def withinLimits(x, y):
+    if 0 <= x < 32 and 0 <= y < 32:
+        return True
+    return False
 
 def main():
     global Matrix
     start_r, start_c = 31, 1
+    API.setWall(0,0,'w')
     current_r, current_c = start_r, start_c
     currentPos = Matrix[current_r][current_c]
-    directions = [(0, -2), (2, 0), (0, 2), (-2, 0)]
+    directions2 = [(-2, 0), (0, 2), (2, 0), (0, -2)]
+    directions1 = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     orientation = 0
+    er, ec = 0, 0
+
+    directionNESW = ['n', 'e', 's', 'w']
+
     while currentPos > 0:
+
         Matrix = floodFill(resetMatrix(Matrix))
         for r,c in centerTiles:
-            API.setText(int((r-1)/2), int((c-1)/2), 0)
-        for row in Matrix:
+            API.setText(int((c-1)/2), 15-int((r-1)/2), 0)
+        """for row in Matrix:
             for item in row:
                 print(f"{str(item):^2}", end="")
-            print()
+            print()"""
 
-        dr, dc = 0, 0
         lowest = 4028
 
-        #TODO: makesure that lowest isnt trying to count tiles beyond edges
-        if API.wallFront():
-            print("front")
-            Matrix[current_r-1][current_c] = hasWallVal
-        elif current_r > 1:
-            lowest = Matrix[current_r-2][current_c]
-            dc, dr = 0, -2
-            tdir = 0 
 
+        o2R, o2C = directions2[orientation]
+        if API.wallFront():
+            oR, oC = directions1[orientation]
+            API.setWall(int((current_c-1)/2), 15-int((current_r-1)/2), directionNESW[orientation])
+            Matrix[current_r+oR][current_c+oC] = hasWallVal
+        elif withinLimits(current_r+o2R, current_c+o2C):
+            print(Matrix[current_r+o2R][current_c+o2C])
+            lowest = Matrix[current_r+o2R][current_c+o2C]
+            dirOffset = 0
+
+
+        o2R, o2C = directions2[(1+orientation)%4]
         if API.wallRight():
-            print("right")
-            Matrix[current_r][current_c+1] = hasWallVal
-        elif current_c < 31:
-            if lowest > Matrix[current_r][current_c+2]:
-                lowest = Matrix[current_r][current_c+2]
-                dc, dr = 2, 0
-                tdir = 1
+            oR, oC = directions1[(1+orientation)%4]
+            API.setWall(int((current_c-1)/2), 15-int((current_r-1)/2), directionNESW[(1+orientation)%4])
+            Matrix[current_r+oR][current_c+oC] = hasWallVal
+        elif withinLimits(current_r+o2R, current_c+o2C): #current_c < 31:
+            if lowest > Matrix[current_r+o2R][current_c+o2C]:
+                lowest = Matrix[current_r+o2R][current_c+o2C]
+                dirOffset = 1
         
+
+        o2R, o2C = directions2[(2+orientation)%4]
         if API.wallBack():
-            print("back")
-            Matrix[current_r+1][current_c] = hasWallVal
-        elif current_r < 31:
-            if lowest > Matrix[current_r+2][current_c]:
-                lowest = Matrix[current_r+2][current_c]
-                dc, dr = 0, 2
-                tdir = 2
+            oR, oC = directions1[(2+orientation)%4]
+            print("back", oR, oC, (2+orientation)%4)
+            API.setWall(int((current_c-1)/2), 15-int((current_r-1)/2), directionNESW[(2+orientation)%4])
+            Matrix[current_r+oR][current_c+oC] = hasWallVal
+            # +1 
+        elif withinLimits(current_r+o2R, current_c+o2C): #current_r < 31:
+            print(Matrix[current_r+o2R][current_c+o2C])
+            if lowest > Matrix[current_r+o2R][current_c+o2C]:
+                lowest = Matrix[current_r+o2R][current_c+o2C]
+                dirOffset = 2
         
+
+        o2R, o2C = directions2[(3+orientation)%4]
         if API.wallLeft():
-            print("left")
-            Matrix[current_r][current_c-1] = hasWallVal
-        elif current_c > 1:
-            if lowest > Matrix[current_r][current_c-2]:
-                lowest = Matrix[current_r][current_c-2]
-                dc, dr = -2, 0
-                tdir = 3
-        
+            oR, oC = directions1[(3+orientation)%4]
+            print("left", oR, oC, (3+orientation)%4)
+            API.setWall(int((current_c-1)/2), 15-int((current_r-1)/2), directionNESW[(3+orientation)%4])
+            Matrix[current_r+oR][current_c+oC] = hasWallVal
+        elif withinLimits(current_r+o2R, current_c+o2C): #current_c > 1:
+            print(Matrix[current_r+o2R][current_c+o2C])
+            if lowest > Matrix[current_r+o2R][current_c+o2C]:
+                lowest = Matrix[current_r+o2R][current_c+o2C]
+                dirOffset = 3
+
+        #TODO FIX BACKTRACKING
+        #TODO ADD ABILITY TO TURN LEFT
         #TODO: ADD DIAGONAL PATH FOLLOWING ABILITIES
-        while orientation != tdir: #TODO: add fastest path algorith/ability 
-            #to turn left or right depending on whats closer to target
+        #orientation = (orientation+tdir)%4
+
+        tOR = (orientation+dirOffset)%4
+        er, ec = directions2[tOR]
+
+        while orientation != tOR:
             API.turnRight90()
             if orientation < 3:
-                orientation += 1
+                orientation+= 1
             else:
                 orientation = 0
-
-        ec, er = directions[orientation]
+        '''
+        for i in range(tdir):
+            API.turnRight90()
+            '''
         API.moveForward()
-        current_r += dr
-        current_c += dc
-        currentPos = Matrix[current_r][current_c]
         API.clearAllText()
+        current_r += er
+        current_c += ec
+        currentPos = Matrix[current_r][current_c]
 
 if __name__ == "__main__":
     main()
+
+"""
+    f   r   b   l
+0   0   1   2   3
+1   1   2   3   0
+2   2   3   0   1
+3   3   0   1   2
+"""
---
+++ mmm-to-run/newMain.py
@@ -0,0 +1,32 @@
+import API
+from collections import deque
+
+m = 16
+n = m
+center = []
+directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+'''
+directions = {
+    'n' : (0, 1),
+    'e' : (1, 0),
+    's' : (0, -1),
+    'w' : (-1, 0)
+}
+'''
+Matrix = [[0000 for x in range(m)] for x in range(n)]
+Visited = [False for _ in ]
+if m % 2 == 0:
+    x = int(m/2)
+    y = int(n/2)
+    center.append((x, y))
+    center.append((x, y-1))
+    center.append((x-1, y))
+    center.append((x-1, y-1))
+else:
+    center.append((int(m//2), int(m/2)))
+print(center)
+
+def inBounds(x, y):
+    return 0 <= x < m and 0 <= y < m
+
+print(inBounds(0, 1))
